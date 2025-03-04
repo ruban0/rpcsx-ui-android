a2f82d48e7dc9845d1873c656642737611d50daa
Muhammad Ashhal <96419703+MAshhal@users.noreply.github.com>
2025-03-05 03:00:55 +0500

    feat: re-write settings screen.

--- app/src/main/java/net/rpcs3/ui/settings/SettingsScreen.kt
+++ app/src/main/java/net/rpcs3/ui/settings/SettingsScreen.kt
@@ -7,85 +7,105 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import net.rpcs3.ui.common.ComposePreview
+import net.rpcs3.ui.settings.components.preference.RegularPreference
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SettingsItem(item: String, onClick: () -> Unit = { }) {
-    ElevatedCard(
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 6.dp
-        ),
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.surfaceVariant,
-        ),
-        onClick = { onClick() },
+fun SettingsScreen(
+    modifier: Modifier = Modifier,
+    navigateBack: () -> Unit
+) {
+    val topBarScrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
         modifier = Modifier
-            .size(width = 320.dp, height = 100.dp)
-            .padding(20.dp)
-    ) {
-        Text(
-            text = item,
+            .nestedScroll(topBarScrollBehavior.nestedScrollConnection)
+            .then(modifier),
+        topBar = {
+            LargeTopAppBar(
+                title = { Text(text = "Settings", fontWeight = FontWeight.Medium) },
+                scrollBehavior = topBarScrollBehavior,
+                navigationIcon = {
+                    IconButton(
+                        onClick = navigateBack
+                    ) {
+                        Icon(imageVector = Icons.AutoMirrored.Default.KeyboardArrowLeft, null)
+                    }
+                }
+            )
+        }
+    ) { contentPadding ->
+        val context = LocalContext.current
+        val scrollState = rememberScrollState()
+
+        var selectedFileUri by remember { mutableStateOf<Uri?>(null) }
+
+        val firmwareFilePicker = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.GetContent()
+        ) { resultUri ->
+            resultUri?.let { selectedFileUri = it }
+            Toast.makeText(context, resultUri.toString(), Toast.LENGTH_SHORT).show()
+        }
+
+        // Create a data class with title and onClick lambda?
+//        val items: List<String> =
+//            remember { mutableListOf("Install Firmware", "Install custom driver") }
+
+        Column(
             modifier = Modifier
-                .padding(16.dp),
-            textAlign = TextAlign.Center,
-        )
+                .fillMaxSize()
+                .padding(contentPadding)
+                .verticalScroll(scrollState)
+        ) {
+            RegularPreference(
+                title = "Install Firmware",
+                leadingIcon = Icons.Default.Settings
+            ) {
+                firmwareFilePicker.launch("*/*")
+            }
+
+            HorizontalDivider()
+
+            RegularPreference(
+                title = "Install Custom Driver",
+                leadingIcon = Icons.Default.Settings
+            ) {
+                /* no-op */
+            }
+        }
     }
 }
 
+@Preview
 @Composable
-fun SettingsScreen() {
-    val items = mutableListOf("Install Firmware", "Install custom driver")
-
-    for (i in 0..10) {
-        items += i.toString()
-    }
-    val scrollState = rememberScrollState()
-    var selectedFileUri by remember { mutableStateOf<Uri?>(null) }
-    val mContext = LocalContext.current
-
-    val pickFileLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.GetContent(),
-        onResult = { uri: Uri? ->
-            selectedFileUri = uri
-            val toast = Toast.makeText(mContext, uri.toString(), Toast.LENGTH_SHORT)
-            toast.show()
-        }
-    )
-
-    Scaffold(
-        modifier = Modifier.padding(vertical = 256.dp)
-    ) { paddingValues ->
-        Column(
-            modifier = Modifier
-                .verticalScroll(scrollState)
-                .fillMaxSize()
-                .padding(paddingValues),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            items.forEach { item ->
-                SettingsItem(item) {
-                    if (item == "Install Firmware") {
-                        pickFileLauncher.launch("*/*")
-                    }
-                }
-            }
-        }
+private fun SettingsScreenPreview() {
+    ComposePreview {
+        SettingsScreen {}
     }
 }
