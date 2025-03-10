804f46396ffc63fb2b07a8fb483df537856c6244
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-03-10 10:50:02 +0530

    RPCS3Activity: enable full screen (#106)

--- app/src/main/java/net/rpcs3/RPCS3Activity.kt
+++ app/src/main/java/net/rpcs3/RPCS3Activity.kt
@@ -2,6 +2,7 @@ package net.rpcs3
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
 
 class RPCS3Activity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,8 +10,25 @@ class RPCS3Activity : Activity() {
         setContentView(R.layout.activity_rpcs3)
 
         listenUsbEvents(this)
+        enableFullScreenImmersive()
 
         val surfaceView = findViewById<GraphicsFrame>(R.id.surfaceView)
         surfaceView.boot(intent.getStringExtra("path")!!)
+    }
+
+    private fun enableFullScreenImmersive() {
+        window.decorView.systemUiVisibility = (
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            or View.SYSTEM_UI_FLAG_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        )
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) enableFullScreenImmersive()
     }
 }
