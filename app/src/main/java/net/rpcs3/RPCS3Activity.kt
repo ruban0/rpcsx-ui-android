30582e3d072b9fc0a125b10299886ab6a2618b6a
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-03-15 02:49:55 +0530

    RPCS3Activity: Add a quick button to hide osc overlay + migrate to viewbinding (#139)
    * Update activity_rpcs3.xml
    
    * Add icons
    
    * Update RPCS3Activity.kt
    
    * RPCS3Activity: migrate to viewbinding
    
    * Update activity_rpcs3.xml

--- app/src/main/java/net/rpcs3/RPCS3Activity.kt
+++ app/src/main/java/net/rpcs3/RPCS3Activity.kt
@@ -3,19 +3,28 @@ package net.rpcs3
 import android.app.Activity
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isInvisible
+import net.rpcs3.R
+import net.rpcs3.databinding.ActivityRpcs3Binding
 
 class RPCS3Activity : Activity() {
+    private lateinit var binding: ActivityRpcs3Binding
     private lateinit var unregisterUsbEventListener: () -> Unit
-
+  
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_rpcs3)
+        binding = ActivityRpcs3Binding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         unregisterUsbEventListener = listenUsbEvents(this)
         enableFullScreenImmersive()
 
-        val surfaceView = findViewById<GraphicsFrame>(R.id.surfaceView)
-        surfaceView.boot(intent.getStringExtra("path")!!)
+        binding.oscToggle.setOnClickListener { 
+            binding.padOverlay.isInvisible = !binding.padOverlay.isInvisible 
+            binding.oscToggle.setImageResource(if (binding.padOverlay.isInvisible) R.drawable.ic_osc_off else R.drawable.ic_show_osc)
+        }
+
+        binding.surfaceView.boot(intent.getStringExtra("path")!!)
     }
 
     override fun onDestroy() {
